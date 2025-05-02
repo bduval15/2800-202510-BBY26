b0de805640339d530bf5e8a6cc930f87544bb94e
Conner685 <cponton@my.bcit.ca>
2025-05-01 17:42:45 -0700

    Installed Supabase dependancies for JavaScript

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -5,3 +5,4 @@ const supaURL = process.env.SUPABASE_URL;
 const publicKey = process.env.PUBLIC_ANON_KEY;
 const serviceKey = process.env.PRIVATE_SERVICE_KEY;
 const tokDecoder = process.env.JWT_TOKEN_DECODER;
+
