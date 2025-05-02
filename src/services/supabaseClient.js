8f75eea0147d0d4ad061e2af69b95cfb74447ab9
Conner685 <cponton@my.bcit.ca>
2025-05-01 17:33:52 -0700

    Installed 'dotenv' package
    Imported Supabase API keys into supabaseClient.js, constants utilizing serverside .env file

---
+++ src/services/supabaseClient.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+// Protected constants
+const supaURL = process.env.SUPABASE_URL; 
+const publicKey = process.env.PUBLIC_ANON_KEY;
+const serviceKey = process.env.PRIVATE_SERVICE_KEY;
+const tokDecoder = process.env.JWT_TOKEN_DECODER;
