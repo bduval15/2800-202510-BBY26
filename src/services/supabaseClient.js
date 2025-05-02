e0705724ccc8310e83a22c68c2602361d60cf42f
Conner685 <cponton@my.bcit.ca>
2025-05-01 17:51:11 -0700

    initialized supabase client to auto refresh and persistent sessions

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,3 +1,5 @@
+const { createClient } = require('@supabase/supabase-js');
+
 require('dotenv').config();
 
 // Protected constants
@@ -6,3 +8,17 @@ const publicKey = process.env.PUBLIC_ANON_KEY;
 const serviceKey = process.env.PRIVATE_SERVICE_KEY;
 const tokDecoder = process.env.JWT_TOKEN_DECODER;
 
+const options = {
+  db: {
+    schema: 'public',
+  },
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionUrl: true
+  },
+  gobal: {
+    headers: {'bby26-supabase': 'LoafLife'},
+  },
+}
+const supabase = createClient(supaURL, publicKey, options);
