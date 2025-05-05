b877ba2a177892095a1572439726501aa930bd7b
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-05-05 16:55:39 -0700

    fix(HacksPage): update hack title in HackCard to reflect accurate information

--- src/app/hacks-page/page.jsx
+++ src/app/hacks-page/page.jsx
@@ -14,7 +14,7 @@ export default function HacksPage() {
         Welcome to Student Hacks! Your posts will appear here.
       </div>
       <HackCard 
-        title="Free McDonald's Coffee"
+        title="BCIT Recreation (Burnaby Campus)"
         upvotes={20}
         downvotes={4}
         comments={7}
